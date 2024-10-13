63e7579321873a7be90b1c269bbce0cd2288a51e
dirceusljr <dirceusljr@gmail.com>
2024-10-13 09:01:31 -0300

    feat: :sparkles: feat: starting project

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {}
